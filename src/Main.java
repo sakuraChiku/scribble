da38ae3bfa72d233cbeb7a054d2197b67b747a1d
sakuraChiku <sakuralee0924@gmail.com>
2026-03-24 23:23:19 +0800

    Deleted some information

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,6 @@
-
-import java.util.ArrayList;
-import scribble.models.*;
-
 public class Main {
     public static void main(String[] args) throws Exception {
-        TileBag bag = new TileBag();
-        System.out.println(bag.tilesRemaining());
-        ArrayList<Tile> rack = new ArrayList<>();
-        for (int i = 0; i < 7; i++) {
-            Tile t = bag.drawTile();
-            rack.add(t);
-        }
-        for (Tile tile : rack) {
-            System.out.print(tile.getLetter() + " " + tile.getScore() + " , ");
-        }
-        System.out.println();
-        System.out.println(bag.tilesRemaining());
+        System.out.println("Hello world");
     }
 }
 // this is a notation made by YYC
